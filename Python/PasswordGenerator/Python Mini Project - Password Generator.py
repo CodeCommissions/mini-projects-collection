821762d1f3807c9b9332dc30dab882975a4e5f7a
matthewzar <funckymat@gmail.com>
2019-03-27 16:16:41 -0400

    Cleaned comments out of jupyter-notebook auto-generated .py file

--- Python/PasswordGenerator/Python Mini Project - Password Generator.py
+++ Python/PasswordGenerator/Python Mini Project - Password Generator.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import string
 def get_char_type(char):
     if(char in string.digits):
@@ -14,10 +8,6 @@ def get_char_type(char):
         return "Upper"
     return "Special"
 
-
-# In[2]:
-
-
 def get_char_type_count(string):
     charCounter = {"Number":0, "Lower":0, "Upper":0, "Special":0}
     for char in string:
@@ -25,18 +15,10 @@ def get_char_type_count(string):
         charCounter[charType] += 1
     return charCounter
 
-
-# In[5]:
-
-
 def calculate_password_strength(password):
     if(len(password) < 8):
         return 0
     return sum(value > 0 for value in get_char_type_count(password).values())
-
-
-# In[17]:
-
 
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 from random import choice, randint
