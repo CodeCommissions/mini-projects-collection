1511709ebf1cf8007d63d0b08d6ba0b06b910d8a
Matthew F <funckymat@gmail.com>
2020-01-28 10:11:45 -0500

    Added homework solution to lesson 2

--- Python/VisualPython-Functions/Lesson2-EnhancedTriangles.py
+++ Python/VisualPython-Functions/Lesson2-EnhancedTriangles.py
@@ -1,7 +1,6 @@
 import turtle
 from numpy import arccos, degrees
 
-
 colors = ["maroon", "purple", "black", "green", "cyan"]
 def setup_turtle():
     turtle.goto(0, -100)
@@ -10,7 +9,7 @@ def setup_turtle():
     turtle.width(5)
 
 def draw_triangle(edge1, edge2 = None, edge3 = None):
-    if(edge2 is None or edge3 is None):
+    if (edge2 is None and edge3 is None):
         edge2 = edge1
         edge3 = edge1
 
@@ -35,7 +34,7 @@ def draw_triangle(edge1, edge2 = None, edge3 = None):
 setup_turtle()
 for i in range(3):
     turtle.color(colors[i])
-    draw_triangle(220, 70, 200)
+    draw_triangle(220, 70)
     turtle.left(120)
 
 turtle.exitonclick()
@@ -46,4 +45,36 @@ quit()
 ######### HOMEWORK SOLUTION BEGINS HERE - AVOID SPOILERS ################
 #########################################################################
 
-#TODO - Add homework
+def draw_triangle_homework_version(edge1, edge2 = None, edge3 = None):
+    '''
+    Draws a triangle. Specifying just 1 edge will draw an equilateral triangle.
+    Including 2 edge-lengths will draw an issolese triangle.
+    Specifying all 3 edges will can draw any type of triangle.
+    '''
+    if (edge2 is None and edge3 is None):
+        edge2 = edge1
+        edge3 = edge1
+
+    #The above check ensures that if once here you can never have more than 1 blank edge
+    if(edge2 is None):
+        edge2 = edge1
+    if(edge3 is None):
+        edge3 = edge1
+
+    #Remember where we started
+    start_pos = turtle.position()
+    start_angle = turtle.heading()
+
+    #Work out how much the turtle should turn
+    temp = (edge1**2 + edge2**2 - edge3**2)/(2*edge1*edge2)
+    radians = arccos(temp)
+    angle = degrees(radians)
+
+    #Move, turn based on that angle, then move the next length
+    turtle.forward(edge1)
+    turtle.right(180 - angle)
+    turtle.forward(edge2)
+
+    #Reset
+    turtle.goto(start_pos)
+    turtle.setheading(start_angle)
